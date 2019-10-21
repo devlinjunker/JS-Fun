9eccc47c54d3cc552df7d652e9ca48bd4f399c94
devlinjunker <devlin.junker@gmail.com>
2019-10-20 17:01:27 -0700

    basic solo swagger setup

---
+++ play/test.swagger/README.md
@@ -0,0 +1,19 @@
+# Exploring Swagger documentation
+
+Exploring Swagger UI with OpenAPI example.
+
+## Dependencies:
+  - Node v8
+  - Babel v7
+  - Webpack v4
+
+
+## Development
+
+Start the webpack dev server and open localhost:8080 to swagger-ui with `npm run dev-server`
+
+
+
+## TODO
+  - [..] Combine with esdoc
+    - [ ] Plugin to build API page and add to header
---
+++ play/test.swagger/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: {
+      index: "./swagger/src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    // Need to do this because path must be absolute
+    path: path.resolve(__dirname, "swagger/")
+  },
+  // Turn off for production (see https://webpack.js.org/guides/production)
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./swagger"
+  },
+  resolve: {
+    extensions: ['.js'],
+    modules: ['node_modules'],
+    alias: {
+      'swagger-css': path.resolve(__dirname, 'node_modules/swagger-ui/dist/swagger-ui.css')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: [{
+          // Use Babel to get ES6 syntax
+          loader: 'babel-loader'
+        }]
+      },
+
+      // Load css files imported
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+
+      // Load images imported
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+}
